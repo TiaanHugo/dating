55c816ab6036648d7721ba4a93a8c35a3a33eae5
Tiaan COMPRSA <tiaan@comprsa.com>
2025-10-27 15:31:23 +0200

    End of Section 7

--- client/src/core/interceptors/error-interceptor.ts
+++ client/src/core/interceptors/error-interceptor.ts
@@ -31,7 +31,8 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
             router.navigateByUrl('/not-found')
             break;
           case 500:
-            const navigationExtra: NavigationExtras = {state: {error: error.error}}
+            const navigationExtras: NavigationExtras = {state: {error: error.error}}
+            router.navigateByUrl('server-error', navigationExtras)
             break;
           default:
             toast.error('Something went wrong');
